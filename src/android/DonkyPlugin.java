ce93be9098b60e5f117e1c693d21e14a1c44b677
Swizzl@ <staven.lepojevic@gmail.com>
2016-05-11 13:17:48 +0100

    andriod push stuff

--- src/android/DonkyPlugin.java
+++ src/android/DonkyPlugin.java
@@ -8,12 +8,23 @@ import android.provider.Settings;
 import org.json.JSONObject;
 import android.content.Context;
 import android.util.Log;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
 
-public class DonkyPlugin extends CordovaPlugin {
+import com.google.android.gms.iid.InstanceID;
+
+
+public class DonkyPlugin extends CordovaPlugin implements PushConstants{
 
     public static final String LOG_TAG = "DonkyPlugin";
     private static CordovaWebView gWebView;
     private static boolean gForeground = false;
+    private static CallbackContext pushContext;
+    private static Bundle gCachedExtras = null;
 
     /**
      * Constructor.
@@ -81,16 +92,16 @@ public class DonkyPlugin extends CordovaPlugin {
      * @return                  True if the action was valid, false if not.
      */    
     @Override
-    public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
+    public boolean execute(String action, final JSONArray data, final CallbackContext callbackContext) throws JSONException {
 
         Log.v(LOG_TAG, "execute: action=" + action);
         gWebView = this.webView;
 
 
         if (action.equals("getPlatformInfo")) {
-            
+
             JSONObject platfornInfo = new JSONObject();
-            
+
             platfornInfo.put("deviceId", getUuid());
             platfornInfo.put("platform", "Android");
             platfornInfo.put("manufacturer", android.os.Build.MANUFACTURER);
@@ -98,12 +109,186 @@ public class DonkyPlugin extends CordovaPlugin {
             platfornInfo.put("version", android.os.Build.VERSION.RELEASE);
 
             callbackContext.success(platfornInfo);
-            return true;            
-        } else {            
+            return true;
+        }
+        else if(action.equals("registerForPush")){
+
+            cordova.getThreadPool().execute(new Runnable() {
+                public void run() {
+                    pushContext = callbackContext;
+                    JSONObject jo = null;
+
+                    Log.v(LOG_TAG, "execute: data=" + data.toString());
+                    SharedPreferences sharedPref = getApplicationContext().getSharedPreferences(COM_ADOBE_PHONEGAP_PUSH, Context.MODE_PRIVATE);
+                    String token = null;
+                    String senderID = null;
+
+                    try {
+                        jo = data.getJSONObject(0).getJSONObject(ANDROID);
+
+                        Log.v(LOG_TAG, "execute: jo=" + jo.toString());
+
+                        senderID = jo.getString(SENDER_ID);
+
+                        Log.v(LOG_TAG, "execute: senderID=" + senderID);
+
+                        String savedSenderID = sharedPref.getString(SENDER_ID, "");
+                        String savedRegID = sharedPref.getString(REGISTRATION_ID, "");
+
+                        // first time run get new token
+                        if ("".equals(savedRegID)) {
+                            token = InstanceID.getInstance(getApplicationContext()).getToken(senderID, GCM);
+                        }
+                        // new sender ID, re-register
+                        else if (!savedSenderID.equals(senderID)) {
+                            token = InstanceID.getInstance(getApplicationContext()).getToken(senderID, GCM);
+                        }
+                        // use the saved one
+                        else {
+                            token = sharedPref.getString(REGISTRATION_ID, "");
+                        }
+
+                        if (!"".equals(token)) {
+                            JSONObject json = new JSONObject().put(REGISTRATION_ID, token);
+
+                            Log.v(LOG_TAG, "onRegistered: " + json.toString());
+
+//                            JSONArray topics = jo.optJSONArray(TOPICS);
+//                            subscribeToTopics(topics, token);
+
+                            DonkyPlugin.sendEvent( json );
+                        } else {
+                            callbackContext.error("Empty registration ID received from GCM");
+                            return;
+                        }
+                    } catch (JSONException e) {
+                        Log.e(LOG_TAG, "execute: Got JSON Exception " + e.getMessage());
+                        callbackContext.error(e.getMessage());
+                    } catch (IOException e) {
+                        Log.e(LOG_TAG, "execute: Got JSON Exception " + e.getMessage());
+                        callbackContext.error(e.getMessage());
+                    }
+
+                    if (jo != null) {
+                        SharedPreferences.Editor editor = sharedPref.edit();
+                        try {
+                            editor.putString(ICON, jo.getString(ICON));
+                        } catch (JSONException e) {
+                            Log.d(LOG_TAG, "no icon option");
+                        }
+                        try {
+                            editor.putString(ICON_COLOR, jo.getString(ICON_COLOR));
+                        } catch (JSONException e) {
+                            Log.d(LOG_TAG, "no iconColor option");
+                        }
+                        editor.putBoolean(SOUND, jo.optBoolean(SOUND, true));
+                        editor.putBoolean(VIBRATE, jo.optBoolean(VIBRATE, true));
+                        editor.putBoolean(CLEAR_NOTIFICATIONS, jo.optBoolean(CLEAR_NOTIFICATIONS, true));
+                        editor.putBoolean(FORCE_SHOW, jo.optBoolean(FORCE_SHOW, false));
+                        editor.putString(SENDER_ID, senderID);
+                        editor.putString(REGISTRATION_ID, token);
+                        editor.commit();
+                    }
+
+                    if (gCachedExtras != null) {
+                        Log.v(LOG_TAG, "sending cached extras");
+                        sendExtras(gCachedExtras);
+                        gCachedExtras = null;
+                    }
+                }
+            });
+
+
+            return true;
+        } else {
             return false;
         }
     }
-    
+
+    public static void sendEvent(JSONObject _json) {
+        PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, _json);
+        pluginResult.setKeepCallback(true);
+        if (pushContext != null) {
+            pushContext.sendPluginResult(pluginResult);
+        }
+    }
+
+    /*
+ * serializes a bundle to JSON.
+ */
+    private static JSONObject convertBundleToJson(Bundle extras) {
+        Log.d(LOG_TAG, "convert extras to json");
+        try {
+            JSONObject json = new JSONObject();
+            JSONObject additionalData = new JSONObject();
+
+            // Add any keys that need to be in top level json to this set
+            HashSet<String> jsonKeySet = new HashSet();
+            Collections.addAll(jsonKeySet, TITLE,MESSAGE,COUNT,SOUND,IMAGE);
+
+            Iterator<String> it = extras.keySet().iterator();
+            while (it.hasNext()) {
+                String key = it.next();
+                Object value = extras.get(key);
+
+                Log.d(LOG_TAG, "key = " + key);
+
+                if (jsonKeySet.contains(key)) {
+                    json.put(key, value);
+                }
+                else if (key.equals(COLDSTART)) {
+                    additionalData.put(key, extras.getBoolean(COLDSTART));
+                }
+                else if (key.equals(FOREGROUND)) {
+                    additionalData.put(key, extras.getBoolean(FOREGROUND));
+                }
+                else if ( value instanceof String ) {
+                    String strValue = (String)value;
+                    try {
+                        // Try to figure out if the value is another JSON object
+                        if (strValue.startsWith("{")) {
+                            additionalData.put(key, new JSONObject(strValue));
+                        }
+                        // Try to figure out if the value is another JSON array
+                        else if (strValue.startsWith("[")) {
+                            additionalData.put(key, new JSONArray(strValue));
+                        }
+                        else {
+                            additionalData.put(key, value);
+                        }
+                    } catch (Exception e) {
+                        additionalData.put(key, value);
+                    }
+                }
+            } // while
+
+            json.put(ADDITIONAL_DATA, additionalData);
+            Log.v(LOG_TAG, "extrasToJSON: " + json.toString());
+
+            return json;
+        }
+        catch( JSONException e) {
+            Log.e(LOG_TAG, "extrasToJSON: JSON exception");
+        }
+        return null;
+    }
+
+    /*
+     * Sends the pushbundle extras to the client application.
+     * If the client application isn't currently active, it is cached for later processing.
+     */
+    public static void sendExtras(Bundle extras) {
+        if (extras != null) {
+            if (gWebView != null) {
+                sendEvent(convertBundleToJson(extras));
+            } else {
+                Log.v(LOG_TAG, "sendExtras: caching extras to send at a later time.");
+                gCachedExtras = extras;
+            }
+        }
+    }
+
+
     /**
      * Get the device's Universally Unique Identifier (UUID).
      *
