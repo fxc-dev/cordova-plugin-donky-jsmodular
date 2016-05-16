6fc05f7e03c74c5c6eacfbff0b337fe7c3bc9ee5
Swizzl@ <staven.lepojevic@gmail.com>
2016-05-16 09:36:40 +0100

    minimal push working on android now

---
+++ src/android/PushInstanceIDListenerService.java
@@ -0,0 +1,10 @@
+package com.donky.plugin;
+
+import com.google.android.gms.iid.InstanceIDListenerService;
+
+public class PushInstanceIDListenerService extends InstanceIDListenerService {
+    @Override
+    public void onTokenRefresh() {
+    }
+
+}
